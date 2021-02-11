dee5e686df3e0c07abfc85952e9d657b3d857581
thrunduil <none@none>
2021-02-11 12:05:11 +0100

    lic update

--- src/blas/blas-loader/plugin_manager.cpp
+++ src/blas/blas-loader/plugin_manager.cpp
@@ -1,7 +1,7 @@
 /*
  *  This file is a part of Matrix Computation Library (MATCL)
  *
- *  Copyright (c) Paweł Kowal 2017 - 2018
+ *  Copyright (c) Paweł Kowal 2017 - 2021
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
